feb2557c7a51ec08d4e567fe670eccd614ef2d60
Roy Kolak <roy.kolak@gmail.com>
2016-09-20 20:14:09 -0500

    integrate macro form

--- public/scripts/modals/edit-display-modal.js
+++ public/scripts/modals/edit-display-modal.js
@@ -3,6 +3,10 @@ import Modal from './modal';
 import DisplayManager from '../managers/display-manager';
 import MacroManager from '../managers/macro-manager';
 import Typewriter from 'typewriter';
+import MacroLibrary from 'macro-library';
+import MacroForm from 'macro-form';
+
+var macroLibrary = new MacroLibrary();
 
 var macroManager = new MacroManager(),
     displayManager = new DisplayManager();
@@ -26,60 +30,60 @@ class EditDisplayModal extends Modal {
               <h4 class="modal-title">Edit Display</h4>
             </div>
             <div class="modal-body">
-              <form>
-                <ul class="nav nav-tabs">
-                  <li class="nav-item">
-                    <a class="nav-link active" data-toggle="tab" href="#edit-general">General</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#edit-owners">Owners</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#edit-macro">Macro</a>
-                  </li>
-                </ul>
-                <div class="tab-content">
-                  <br />
-                  <div id="edit-general" class="tab-pane active">
-                    <div class="row">
-                      <div class="col-xs-12">
-                        <fieldset class="form-group">
-                          <label for="display-name">Display name</label>
-                          <input type="name" name="display-name" class="form-control" id="display-name" />
-                        </fieldset>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-xs-12 col-sm-6">
-                        <fieldset class="form-group">
-                          <label for="display-width">Select width</label>
-                          <select class="form-control" id="display-width" name="width">
-                            <option value="16">16</option>
-                            <option value="32">32</option>
-                            <option value="64">64</option>
-                            <option value="96">96</option>
-                            <option value="128">128</option>
-                          </select>
-                        </fieldset>
-                      </div>
-                      <div class="col-xs-12 col-sm-6">
-                        <fieldset class="form-group">
-                          <label for="display-height">Select height</label>
-                          <select class="form-control" id="display-height" name="height">
-                            <option value="16">16</option>
-                            <option value="32">32</option>
-                            <option value="64">64</option>
-                            <option value="96">96</option>
-                            <option value="128">128</option>
-                          </select>
-                        </fieldset>
-                      </div>
+              <ul class="nav nav-tabs">
+                <li class="nav-item">
+                  <a class="nav-link active" data-toggle="tab" href="#edit-general">General</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#edit-owners">Owners</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#edit-macro">Macro</a>
+                </li>
+              </ul>
+              <div class="tab-content">
+                <br />
+                <div id="edit-general" class="tab-pane active">
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <fieldset class="form-group">
+                        <label for="display-name">Display name</label>
+                        <input type="name" name="display-name" class="form-control" id="display-name" />
+                      </fieldset>
                     </div>
                   </div>
-                  <div id="edit-owners" class="tab-pane">
-                    <ul id="display-owners" class="list-group"></ul>
+                  <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+                      <fieldset class="form-group">
+                        <label for="display-width">Select width</label>
+                        <select class="form-control" id="display-width" name="width">
+                          <option value="16">16</option>
+                          <option value="32">32</option>
+                          <option value="64">64</option>
+                          <option value="96">96</option>
+                          <option value="128">128</option>
+                        </select>
+                      </fieldset>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                      <fieldset class="form-group">
+                        <label for="display-height">Select height</label>
+                        <select class="form-control" id="display-height" name="height">
+                          <option value="16">16</option>
+                          <option value="32">32</option>
+                          <option value="64">64</option>
+                          <option value="96">96</option>
+                          <option value="128">128</option>
+                        </select>
+                      </fieldset>
+                    </div>
                   </div>
-                  <div id="edit-macro" class="tab-pane">
+                </div>
+                <div id="edit-owners" class="tab-pane">
+                  <ul id="display-owners" class="list-group"></ul>
+                </div>
+                <div id="edit-macro" class="tab-pane">
+                  <form id="macro">
                     <div class="row">
                       <div class="col-xs-12">
                         <fieldset class="form-group">
@@ -88,108 +92,12 @@ class EditDisplayModal extends Modal {
                         </fieldset>
                       </div>
                     </div>
-                    <div class="programmable options row" style="display: none;">
-                      <div class="col-xs-12">
-                        <p class="programmable description"></p>
-                        <p>Warning you need programming skills to use this display macro. Learn more about this option <a href="#">here.</a>
-                      </div>
-                    </div>
-                    <div class="twinkle options row" style="display: none;">
-                      <div class="col-xs-12">
-                        <p class="twinkle description"></p>
-                        <fieldset class="form-group">
-                          <h5>Macro options</h5>
-                          <label for="twinkle-base-color">Seed Color</label>
-                          <div class="input-group colorpicker-component">
-                            <input type="text" id="twinkle-seed-color" value="#006e91" class="form-control" />
-                            <span class="input-group-addon"><i></i></span>
-                          </div>
-                          <small class="text-muted">The brightest hex value you want to display</small>
-                        </fieldset>
-                      </div>
-                    </div>
-                    <div class="solid-color options row" style="display: none;">
-                      <div class="col-xs-12">
-                        <p class="solid-color description"></p>
-                        <fieldset class="form-group">
-                          <h5>Macro options</h5>
-                          <label for="solid-color">Color</label>
-                          <div class="input-group colorpicker-component">
-                            <input type="text" id="solid-color" value="#006e91" class="form-control" />
-                            <span class="input-group-addon"><i></i></span>
-                          </div>
-                        </fieldset>
-                      </div>
-                    </div>
-                    <div class="marquee options row" style="display: none;">
-                      <div class="col-xs-12">
-                        <p class="marquee description"></p>
-                        <div class="row">
-                          <div class="col-xs-12">
-                            <h5>Macro options</h5>
-                            <div class="form-group">
-                              <label for="solid-color">Color</label>
-                              <div class="input-group colorpicker-component">
-                                <input type="text" id="marquee-color" value="#006e91" class="form-control" />
-                                <span class="input-group-addon"><i></i></span>
-                              </div>
-                            </div>
-                            <div class="form-group">
-                              <label for="marquee-value">Text</label>
-                              <input type="text" id="marquee-value" placeholder="What you want displayed..." class="form-control" />
-                            </div>
-                            <div class="form-group">
-                              <label for="marquee-font">Select font</label>
-                              <select class="form-control fonts" id="marquee-fonts"></select>
-                            </div>
-                            <div class="form-group">
-                              <label for="marquee-speed">Marquee speed</label>
-                              <select class="form-control" id="marquee-speed" name="speed">
-                                <option value="1">1</option>
-                                <option value="10">10</option>
-                                <option value="50">50</option>
-                                <option value="100">100</option>
-                                <option value="250">250</option>
-                                <option value="500">500</option>
-                              </select>
-                              <p class="form-text text-muted">
-                                The speed the text is scrolling, in milliseconds
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="text options row" style="display: none;">
-                      <div class="col-xs-12">
-                        <p class="text description"></p>
-                        <div class="row">
-                          <div class="col-xs-12">
-                            <h5>Macro options</h5>
-                            <div class="form-group">
-                              <label for="solid-color">Color</label>
-                              <div class="input-group colorpicker-component">
-                                <input type="text" id="text-color" value="#006e91" class="form-control" />
-                                <span class="input-group-addon"><i></i></span>
-                              </div>
-                            </div>
-                            <div class="form-group">
-                              <label for="text-value">Text</label>
-                              <input type="text" id="text-value" placeholder="What you want displayed..." class="form-control" />
-                            </div>
-                            <div class="form-group">
-                              <label for="text-font">Select font</label>
-                              <select class="form-control fonts" id="text-fonts"></select>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                    <div id="macro-form"></div>
+                    <button type="submit" class="btn btn-success">Update</button>
+                  </form>
                 </div>
-                <br /><br />
-                <button type="submit" class="btn btn-success">Update</button>
-              </form>
+              </div>
+              <br /><br />
             </div>
           </div>
         </div>
@@ -198,7 +106,6 @@ class EditDisplayModal extends Modal {
 
     this.populateMacros();
     this.populateOwners();
-    this.populateFonts();
 
     this.$('#edit-display').on('show.bs.modal', () => {
       this.$('select#macro').val(this.displayData.macro).change();
@@ -207,65 +114,23 @@ class EditDisplayModal extends Modal {
     });
     this.$('#display-name').val(this.displayData.name)
 
-    this.$('.colorpicker-component').colorpicker();
 
-    var $twinkleOptions = this.$('.options.twinkle'),
-        $programmableOptions = this.$('.options.programmable'),
-        $solidColorOptions = this.$('.options.solid-color'),
-        $marqueeOptions = this.$('.options.marquee'),
-        $textOptions = this.$('.options.text');
+    var macroForm;
 
-    this.$('select#macro').change(function(el) {
-      $twinkleOptions.hide();
-      $programmableOptions.hide();
-      $solidColorOptions.hide();
-      $marqueeOptions.hide();
-      $textOptions.hide();
-
-      if(this.value === 'twinkle') {
-        $twinkleOptions.show();
-      } else if(this.value == 'programmable') {
-        $programmableOptions.show();
-      } else if(this.value == 'solid-color') {
-        $solidColorOptions.show();
-      } else if(this.value == 'text') {
-        $textOptions.show();
-      } else if(this.value == 'marquee') {
-        $marqueeOptions.show();
-      }
+    this.$('select#macro').change((el) => {
+      var selectedMacro = $(el.currentTarget).find(':selected').val();
+      var config = macroLibrary.macroConfig(selectedMacro);
+      macroForm = new MacroForm($('#macro-form'), selectedMacro, config);
+      macroForm.render();
+      this.$('.colorpicker-component').colorpicker();
     });
 
-    this.$('form').submit((ev) => {
+    this.$('form#macro').submit((ev) => {
       ev.preventDefault();
-
       var newData = {
-        macro: this.$('select#macro').val(),
-        name: this.$('#display-name').val(),
-      };
-
-      if(newData.macro === 'twinkle') {
-        newData.macroConfig = {
-          seedColor: this.$('#twinkle-seed-color').val()
-        };
-      } else if(newData.macro === 'solid-color') {
-        newData.macroConfig = {
-          color: this.$('#solid-color').val()
-        };
-      } else if(newData.macro === 'text') {
-        newData.macroConfig = {
-          color: this.$('#text-color').val(),
-          text: this.$('#text-value').val().toUpperCase(),
-          font: this.$('#text-fonts').val()
-        }
-      } else if(newData.macro === 'marquee') {
-        newData.macroConfig = {
-          color: this.$('#marquee-color').val(),
-          text: this.$('#marquee-value').val().toUpperCase(),
-          font: this.$('#marquee-fonts').val(),
-          marqueeSpeed: this.$('#marquee-speed').val()
-        }
+        macroConfig: macroForm.serialData(),
+        macro: macroForm.macro
       }
-
       displayManager.update(this.displayKey, newData, (displayKey) => {
         this.$('#edit-display').modal('hide');
 
@@ -285,13 +150,6 @@ class EditDisplayModal extends Modal {
         $macrosSelect.append(`<option value=${key}>${macros[key].name}</option>`);
         this.$(`.description.${key}`).text(macros[key].description);
       }
-    });
-  }
-
-  populateFonts() {
-    var $fontsSelect = this.$('select.fonts');
-    Typewriter.availableFonts().forEach((font) => {
-      $fontsSelect.append(`<option value=${font}>${font}</option>`);
     });
   }
 
