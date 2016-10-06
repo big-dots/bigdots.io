dc7540271b62bae3666146b6c77f2415c48e4453
Roy Kolak <roy.kolak@gmail.com>
2016-10-05 20:07:38 -0500

    move api instructions to github

--- public/scripts/pages/display-page.js
+++ public/scripts/pages/display-page.js
@@ -2,7 +2,6 @@ import DisplayCoupler from 'display-coupler';
 import DotMatrix from 'dot-matrix';
 import Page from './page';
 import EditDisplayModal from '../modals/edit-display-modal';
-import ApiUsageModal from '../modals/api-usage-modal';
 import DisplayManager from '../managers/display-manager';
 
 var displayManager = new DisplayManager();
@@ -25,12 +24,23 @@ class DisplayPage extends Page {
         </div>
         <div class='matrix-container'></div>
         <div class="display-meta" style="display: none;">
-          <a href="#" class="btn btn-link pull-right api-usage" data-toggle="modal" data-target="#api-usage">
-            Using the API...
+          <a href="https://github.com/bigdots-io/bigdots-io-node/" class="btn btn-link pull-right">
+            Node.js
           </a>
+          <a href="https://github.com/bigdots-io/bigdots-io-http/" class="btn btn-link pull-right">
+            HTTP
+          </a>
+          <p class="pull-right">Update with..</p>
         </div>
         <div class="edit-display-modal"></div>
         <div class="api-usage-modal"></div>
+      </div>
+      <div style="position: absolute; bottom: 100px; right: 0; left: 0; height: 100px; text-align: center;">
+        <i class="fa fa-user-secret" aria-hidden="true" style="font-size: 50px; color: #AAA; display: inline-block;"></i>
+        <div style="display: inline-block; text-align: left;">
+          <i>Be careful who you share this page with...</i><br />
+          <strong>${this.id}</strong> is your API secret!
+        </div>
       </div>
     `);
 
@@ -66,9 +76,6 @@ class DisplayPage extends Page {
 
       var $editDisplayModal = this.$('.edit-display-modal');
       new EditDisplayModal($editDisplayModal, this.id, displayData).render();
-
-      var $apiUsageModal = this.$('.api-usage-modal');
-      new ApiUsageModal($apiUsageModal, this.id, displayData).render();
     });
   }
 }
