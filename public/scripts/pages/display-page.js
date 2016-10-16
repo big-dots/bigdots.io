9a9763957a865c9f8166fd6569717ab661763259
Roy Kolak <roy.kolak@gmail.com>
2016-10-16 16:16:49 -0500

    simplify

--- public/scripts/pages/display-page.js
+++ public/scripts/pages/display-page.js
@@ -1,7 +1,6 @@
 import DisplayCoupler from 'display-coupler';
 import DotMatrix from 'dot-matrix';
 import Page from './page';
-import EditDisplayModal from '../modals/edit-display-modal';
 import DisplayManager from '../managers/display-manager';
 
 var displayManager = new DisplayManager();
@@ -16,10 +15,6 @@ class DisplayPage extends Page {
     this.$el.html(`
       <div class="frame" style="display: none;">
         <div class="display-meta" style="display: none;">
-          <a href="#" class="btn btn-link pull-right change-macro" data-toggle="modal" data-target="#edit-display">
-            <span class="display-macro"></span>
-            <i class="fa fa-pencil"></i>
-          </a>
           <span class="display-name text-left"></span>
         </div>
         <div class='matrix-container'></div>
@@ -32,8 +27,6 @@ class DisplayPage extends Page {
           </a>
           <p class="pull-right">Update with..</p>
         </div>
-        <div class="edit-display-modal"></div>
-        <div class="api-usage-modal"></div>
       </div>
       <div style="position: absolute; bottom: 100px; right: 0; left: 0; height: 100px; text-align: center;">
         <i class="fa fa-user-secret" aria-hidden="true" style="font-size: 50px; color: #AAA; display: inline-block;"></i>
@@ -62,9 +55,6 @@ class DisplayPage extends Page {
 
       var displayCoupler = new DisplayCoupler(firebase.database());
       displayCoupler.connect(this.id, {
-        onReady: function(displayData, next) {
-          next()
-        },
         onPixelChange: (y, x, hex) => {
           dotMatrix.updateDot(y, x, hex);
         }
@@ -73,9 +63,6 @@ class DisplayPage extends Page {
       this.$('.display-name').text(displayData.name);
       this.$('.display-macro').text(displayData.macro);
       this.$('.frame').fadeIn();
-
-      var $editDisplayModal = this.$('.edit-display-modal');
-      new EditDisplayModal($editDisplayModal, this.id, displayData).render();
     });
   }
 }
